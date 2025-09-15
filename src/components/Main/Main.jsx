a902f71155a50128d27679f6286b431a84efab3d
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-09-15 10:51:35 +0200

    Tilføjet både main content og navbar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
+import Hero from "../../assets/cars-and-trees.svg"
+import Footer from "../../assets/Footer.png"
+import styles from "./Main.module.scss"
 
 export const Main = () => {
   return (
-    <main>
-        indhold
+    <main className={styles.main}>
+        <img className={styles.image} src={Hero} alt="Hero" />
+        <img className={`${styles.image} ${styles.imageFooter}`} src={Footer} alt="Footer" />
     </main>
   )
 }
