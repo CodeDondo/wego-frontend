21eaebd8334d30aa33f07d17332a64c1fd81ccca
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-09-16 11:07:23 +0200

    små justering på tekst

--- src/components/HowToUse/HowToUse.jsx
+++ src/components/HowToUse/HowToUse.jsx
@@ -6,8 +6,7 @@ export const HowToUse = ({ isVisible, onClose, isMobile }) => {
       <section className={styles.howToUse}>
         <h1>Sådan virker det</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          aliquam leo libero, vitae ullamcorper nunc rutrum sit amet...
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam leo libero, vitae ullamcorper nunc rutrum sit amet. Integer lobortis diam eu justo fermentum, lacinia laoreet urna efficitur. Mauris sit amet urna vulputate, vulputate turpis a, interdum elit. Fusce quis rutrum odio. Integer nec euismod felis. Praesent ex justo, ultrices a neque in, facilisis condimentum ex. Cras iaculis eget nulla a vestibulum. Donec suscipit eu nunc in dictum. Vestibulum congue scelerisque velit, ut tempus urna dictum eu. Morbi id nisi risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer feugiat dui id elit placerat, at volutpat nunc rhoncus.
         </p>
       </section>
     )
@@ -21,8 +20,7 @@ export const HowToUse = ({ isVisible, onClose, isMobile }) => {
         <button className={styles.close} onClick={onClose}>×</button>
         <h1>Sådan virker det</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          aliquam leo libero, vitae ullamcorper nunc rutrum sit amet...
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam leo libero, vitae ullamcorper nunc rutrum sit amet. Integer lobortis diam eu justo fermentum, lacinia laoreet urna efficitur. Mauris sit amet urna vulputate, vulputate turpis a, interdum elit. Fusce quis rutrum odio. Integer nec euismod felis. Praesent ex justo, ultrices a neque in, facilisis condimentum ex. Cras iaculis eget nulla a vestibulum. Donec suscipit eu nunc in dictum. Vestibulum congue scelerisque velit, ut tempus urna dictum eu. Morbi id nisi risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer feugiat dui id elit placerat, at volutpat nunc rhoncus.
         </p>
       </section>
     </div>
